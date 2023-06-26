e7fcbde869b709e9283fc1140fac61241d586b47
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-06-26 11:44:34 +0200

    feat: highlight banner finished

--- src/app/components/organism/highlight-banner/highlight-banner.component.ts
+++ src/app/components/organism/highlight-banner/highlight-banner.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-highlight-banner',
   templateUrl: './highlight-banner.component.html',
-  styleUrls: ['./highlight-banner.component.scss']
+  styleUrls: ['./highlight-banner.component.scss'],
 })
 export class HighlightBannerComponent {
-
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() imageUrl: string = '';
 }
