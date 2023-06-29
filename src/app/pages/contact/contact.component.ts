c79e668c0fcdf71855e3f50ae2109ca3f63361a7
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-06-29 08:03:10 +0200

    feat: dynamic contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'main[app-contact].page-container',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  email = 'contato@sitedabanda.com.br';
+  whatsappNumber = '21991015627';
+  whatsappUrl = `https://api.whatsapp.com/send/?phone=%2B55${this.whatsappNumber}`;
 
+  ngOnInit() {
+    this.whatsappNumber = `(${this.whatsappNumber.slice(
+      0,
+      2
+    )}) ${this.whatsappNumber.slice(2, 7)}-${this.whatsappNumber.slice(7, 11)}`;
+  }
 }
