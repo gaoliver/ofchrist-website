2983772bf552207b7ec78b472806c347102161b1
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-06-29 19:38:52 +0200

    feat: Home page featured video redesigned

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { mockVideos } from '@src/app/@dummyData';
 import { HomePromo } from '@src/app/@types/types';
-import { videoIdExtractor } from '@src/app/utils/videoIdExtractor';
+import { FeaturedMusicVideo } from '@src/app/components/@types/types';
 import { env } from 'src/environments/environment';
 
 @Component({
@@ -11,33 +12,44 @@ import { env } from 'src/environments/environment';
 export class HomeComponent implements OnInit {
   showVideo = `${env.baseUrl}/assets/videos/show-caverna-cut.mp4`;
   promo: HomePromo | undefined;
-  videoHeight = 420;
-  videoWidth = 700;
-
-  getPlayerSize() {
-    const player = document.getElementById('youtube-player');
-
-    if (player) {
-      this.videoWidth = player.offsetHeight;
-      this.videoHeight = player.offsetWidth;
-    }
-  }
+  videoRelease: FeaturedMusicVideo | undefined;
 
   ngOnInit() {
     // this.promo = {
     //   name: 'lançamento do álbum "Confiança PT.2"',
     //   image: 'https://i.scdn.co/image/ab67616d0000b2735229e5fdd5c833e8b8620e70',
-    //   videoUrl: 'https://www.youtube.com/watch?v=-XU_JybF_tU',
+    //   videoUrl: '',
     //   cta: {
     //     label: 'Ouça agora!',
     //     href: '#',
     //   },
     // };
 
-    // if (this.promo.videoUrl) {
-    //   this.promo.videoUrl = videoIdExtractor(this.promo.videoUrl);
-    // }
-
-    this.getPlayerSize();
+    // this.videoRelease = {
+    //   title: 'Novo Lançamento Musical',
+    //   url: mockVideos[0].videoUrl,
+    //   streaming: [
+    //     {
+    //       icon: 'spotify',
+    //       href: '#',
+    //       label: 'Spotify',
+    //     },
+    //     {
+    //       icon: 'deezer',
+    //       href: '#',
+    //       label: 'Deezer',
+    //     },
+    //     {
+    //       icon: 'apple-music',
+    //       href: '#',
+    //       label: 'Apple Music',
+    //     },
+    //     {
+    //       icon: 'youtube-music',
+    //       href: '#',
+    //       label: 'YouTube Music',
+    //     },
+    //   ],
+    // };
   }
 }
