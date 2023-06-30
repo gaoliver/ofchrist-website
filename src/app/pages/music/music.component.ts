83424ddfdd749a32eec3dc94cfcfd50ed0eb411e
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-06-30 17:40:45 +0200

    feat: Discography on Music page

--- src/app/pages/music/music.component.ts
+++ src/app/pages/music/music.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { mockAlbums } from '@src/app/@dummyData';
 import { mockVideos } from '@src/app/@dummyData/media';
+import {
+  FeaturedBanner,
+  FeaturedMusicVideo,
+} from '@src/app/components/@types/types';
+import { env } from '@src/environments/environment';
 
 @Component({
   selector: 'main[app-music].page-container',
@@ -8,5 +14,37 @@ import { mockVideos } from '@src/app/@dummyData/media';
 })
 export class MusicComponent {
   videos = mockVideos.shift();
-  featuredVideo = mockVideos[0];
+
+  featuredVideo: FeaturedMusicVideo = {
+    url: mockVideos[0].videoUrl,
+    title: 'Música nova da banda',
+    streaming: [
+      {
+        icon: 'spotify',
+        href: '#',
+        label: 'Spotify',
+      },
+      {
+        icon: 'deezer',
+        href: '#',
+        label: 'Deezer',
+      },
+      {
+        icon: 'apple-music',
+        href: '#',
+        label: 'Apple Music',
+      },
+      {
+        icon: 'youtube-music',
+        href: '#',
+        label: 'YouTube Music',
+      },
+    ],
+  };
+
+  albums: FeaturedBanner[] = mockAlbums.map((album) => ({
+    href: env.baseUrl + '/musicas/album/' + album.id,
+    title: album.title,
+    imageUrl: album.cover,
+  }));
 }
