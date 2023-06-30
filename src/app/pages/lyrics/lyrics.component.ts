8e1e4f9f44d4abcf6d25c060057c0099ae96259a
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-06-30 08:54:05 +0200

    feat: dynamic lyrics list page

--- src/app/pages/lyrics/lyrics.component.ts
+++ src/app/pages/lyrics/lyrics.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { mockLyrics } from '@src/app/@dummyData';
+import { SongLyrics } from '@src/app/@types/types';
 
 @Component({
   selector: 'main[app-lyrics].page-container',
   templateUrl: './lyrics.component.html',
   styleUrls: ['./lyrics.component.scss'],
 })
-export class LyricsComponent {}
+export class LyricsComponent implements OnInit {
+  songList = mockLyrics;
+  groupedSongsList: { key: string; songs: SongLyrics[] }[] = [];
+
+  sortAndGroupByLetter(list: SongLyrics[]) {
+    const sortedList = list.sort((a, b) => a.title.localeCompare(b.title));
+    const groupedByLetter: typeof this.groupedSongsList = [];
+
+    sortedList.forEach((song) => {
+      const letter = song.title.charAt(0).toLowerCase();
+
+      const songsGroup = groupedByLetter.find((group) => group.key === letter);
+
+      if (!songsGroup) {
+        groupedByLetter.push({
+          key: letter,
+          songs: [song],
+        });
+      } else {
+        songsGroup.songs.push(song);
+      }
+    });
+
+    return groupedByLetter;
+  }
+
+  ngOnInit() {
+    this.groupedSongsList = this.sortAndGroupByLetter(this.songList);
+  }
+}
