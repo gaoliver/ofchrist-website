ba11387132a8bfe5c09c341db7c8cf1bcb5d61f7
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-07-07 09:32:24 +0200

    feat(Component): :sparkles: Footer
    Integration with Contentful API

--- src/app/components/organism/footer/footer.component.ts
+++ src/app/components/organism/footer/footer.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Link, SocialNetwork } from '@components/@types/types';
+import { Store, select } from '@ngrx/store';
+import { Home } from '@src/app/@types/types';
+import { AppState } from '@src/store/app.state';
+import { getHomeSelector } from '@src/store/home/home.selectors';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: '[app-footer]',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss', '../../../../styles.scss'],
 })
-export class FooterComponent {
-  currYear = new Date().getFullYear();
+export class FooterComponent implements OnInit {
+  currYear: number | undefined;
+  socialNetworks: Array<SocialNetwork> | undefined;
+  streamingPlatforms: Array<SocialNetwork> | undefined;
+  home$: Observable<Home> | undefined;
 
   footerLink: Link | undefined = {
     label: 'Desenvolvido por Gabriel Ramos',
@@ -15,49 +23,20 @@ export class FooterComponent {
     target: '_blank',
   };
 
-  socialNetworks: Array<SocialNetwork> = [
-    {
-      label: 'YouTube',
-      href: 'https://www.youtube.com/@BandaOfChrist',
-      icon: 'youtube',
-    },
-    {
-      label: 'Instagram',
-      href: 'https://www.instagram.com/ofchristrock/',
-      icon: 'instagram',
-    },
-    {
-      label: 'Facebook',
-      href: 'https://www.facebook.com/bandaofchrist/',
-      icon: 'facebook',
-    },
-    {
-      label: 'TikTok',
-      href: 'https://www.tiktok.com/@ofchristofficial',
-      icon: 'tiktok',
-    },
-  ];
+  constructor(private store: Store<AppState>) {
+    this.currYear = new Date().getFullYear();
+  }
 
-  streamingPlatforms: Array<SocialNetwork> = [
-    {
-      label: 'Spotify',
-      href: 'https://open.spotify.com/artist/1LYah3vtwsLKZbU6fca4ks',
-      icon: 'spotify',
-    },
-    {
-      label: 'Deezer',
-      href: 'https://www.deezer.com/en/artist/52807832',
-      icon: 'deezer',
-    },
-    {
-      label: 'Apple Music',
-      href: 'https://music.apple.com/nl/artist/of-christ/1505133392?l=en',
-      icon: 'apple-music',
-    },
-    {
-      label: 'YouTube Music',
-      href: 'https://music.youtube.com/channel/UC4lsLSYY4kW5Jvg5WJYCtEg',
-      icon: 'youtube-music',
-    },
-  ];
+  getFooterIcons() {
+    this.home$?.subscribe((home) => {
+      this.socialNetworks = home.social_networks;
+      this.streamingPlatforms = home.streaming;
+    });
+  }
+
+  ngOnInit(): void {
+    this.home$ = this.store.pipe(select(getHomeSelector));
+
+    this.getFooterIcons();
+  }
 }
