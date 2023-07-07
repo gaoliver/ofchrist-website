bf464f4b2e7e1e6edf6200c28199b0fbb1aa967f
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-07-07 08:58:01 +0200

    feat(Component): :sparkles: Sidebar & Footer
    Scaled up icons and added Social Networks to Sidebar menu

--- src/app/components/organism/sidemenu/sidemenu.component.ts
+++ src/app/components/organism/sidemenu/sidemenu.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { pageRoutes } from '@src/app/app-routing.module';
+import { SocialNetwork } from '../../@types/types';
+import { Store, select } from '@ngrx/store';
+import { AppState } from '@src/store/app.state';
+import { Observable } from 'rxjs';
+import { Home } from '@src/app/@types/types';
+import { getHomeSelector } from '@src/store/home/home.selectors';
 
 @Component({
   selector: 'app-sidemenu',
@@ -8,11 +14,26 @@ import { pageRoutes } from '@src/app/app-routing.module';
 })
 export class SidemenuComponent implements OnInit {
   pageList = pageRoutes;
+  socialNetworks: SocialNetwork[] | undefined;
+  home$: Observable<Home> | undefined;
+
+  constructor(private store: Store<AppState>) {}
+
+  getFromStore() {
+    this.home$ = this.store.pipe(select(getHomeSelector));
+  }
 
   closeMenu() {
     const menu = document.getElementById('mobile-menu');
     menu?.classList.remove('active-menu');
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getFromStore();
+
+    this.home$?.subscribe((home) => {
+      this.socialNetworks = home.social_networks;
+      console.log(home.social_networks);
+    });
+  }
 }
--- src/store/home/home.service.ts
+++ src/store/home/home.service.ts
@@ -33,8 +33,6 @@ export class HomeService {
       },
     };
 
-    console.log(home);
-
     return home;
   }
 }
